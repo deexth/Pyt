86b5e23af9ff11484f451b8e6df254bbc9b8e343
deexth <hkabare@gmail.com>
2022-06-15 18:24:13 +0200

    New changes to codewars

--- codewars/digital_root_kata6.py
+++ codewars/digital_root_kata6.py
@@ -1,16 +1,20 @@
-def digital_root(x):
-    if x < 10:
-        return x;
-    x = x%10 + digital_root(x//10)
-    return digital_root(x)
+# def digital_root(x):
+#     if x < 10:
+#         return x;
+#     x = x%10 + digital_root(x//10)
+#     return digital_root(x)
     
+
+
+
+def digital_root(n):
+    root = 0
+    for d in str(n):
+        root += int(d)
+    if len(str(root)) > 1:
+        root = digital_root(root)
+    return root
+
+
+
 digital_root(942)
-
-
-# def digital_root(n):
-#     root = 0
-#     for d in str(n):
-#         root += int(d)
-#     if len(str(root)) > 1:
-#         root = digital_root(root)
-#     return root
